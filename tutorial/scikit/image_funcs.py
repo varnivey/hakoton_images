2ad79b833c47f73fad55d8d634399b600994dd1a
varnivey <varnivey@mail.ru>
2015-04-11 00:52:49 +0300

    Scikit-image tutorial added

---
+++ tutorial/scikit/image_funcs.py
@@ -0,0 +1,131 @@
+import numpy as np
+from skimage.measure import label
+
+from skimage.filter import gaussian_filter, canny
+from skimage.morphology import binary_dilation, binary_erosion
+from skimage.morphology import remove_small_objects
+
+from scipy.ndimage import distance_transform_edt
+from skimage.feature import peak_local_max
+from skimage.morphology import watershed
+
+def fill_holes(binary):
+    '''Fill holes in binary image'''
+
+    labels = label(binary)
+
+    labelcount = np.bincount(labels.ravel())
+
+    bg = np.argmax(labelcount)
+
+    binary[labels != bg] = True
+
+    return binary
+
+
+def clear_border(labels):
+    '''Removes objects which touch the border'''
+
+    borders = np.zeros(labels.shape, np.bool)
+
+    borders[ 0,:] = 1
+    borders[-1,:] = 1
+    borders[:, 0] = 1
+    borders[:,-1] = 1
+
+    at_border = np.unique(labels[borders])
+
+    for obj in at_border:
+        labels[labels == obj] = 0
+
+    return labels
+
+def double_dilation(binary, selem):
+    '''Returns the result of two sequential binary dilations'''
+
+    for i in (1,2):
+        binary = binary_dilation(binary, selem)
+
+    return binary
+
+def double_erosion(binary, selem):
+    '''Returns the result of two sequential binary erosions'''
+
+    for i in (1,2):
+        binary = binary_erosion(binary, selem)
+
+    return binary
+
+def colorize(labels):
+    '''Colorize cells in such a way that neighbour cells have distict colors'''
+
+    label_nums = np.unique(labels)
+    new_image = np.zeros_like(labels, dtype=np.float)
+
+    for i in range(label_nums.size):
+        if i == 0:
+            continue
+        new_image[labels == label_nums[i]] = i*0.27
+
+    new_image = (new_image % 1.)*0.7 + 0.3
+    new_image[labels == label_nums[0]] = 0.
+
+    return new_image
+
+
+def get_binary(cells):
+    '''Returns binary image with cells'''
+
+    if (len(cells.shape) == 3):
+        cells = np.max(cells, 2)
+
+    blured = gaussian_filter(cells,8)
+    highpass = cells - 0.8*blured
+    sharp = highpass + cells
+    sharp = np.floor(sharp).astype(np.uint8)
+
+    diamond = np.array([0,1,0,1,1,1,0,1,0], dtype=bool).reshape((3,3))
+
+    edges = canny(sharp, sigma = 1, high_threshold = 35., low_threshold = 14.)
+    edges = double_dilation(edges, diamond)
+    binary = fill_holes(edges)
+    binary = double_erosion(binary, diamond)
+
+    return binary
+
+
+def calc_prot_intensity(labels, prot_pic):
+    '''Return an array of protein intensities inside cells'''
+
+    result = []
+
+    for label in np.unique(labels):
+        if label == 0:
+            continue
+        prot_values = np.extract(labels == label,prot_pic)
+        result.append(np.mean(prot_values))
+
+    return result
+
+
+def get_labels(binary):
+    '''Return image with labeled cells'''
+
+    distance = distance_transform_edt(binary)
+    distance_blured = gaussian_filter(distance, 8)
+    local_maxi = peak_local_max(distance_blured, indices=False, labels=binary, min_distance = 10)
+    markers = label(local_maxi)
+    labels = watershed(-distance, markers, mask=binary)
+    labels = remove_small_objects(labels, 4000)
+    labels = clear_border(labels)
+    return labels
+
+def get_cell_number(cells):
+    '''Return number of cells in the image given'''
+
+    binary = get_binary(cells)
+    labels = get_labels(binary)
+
+    return np.unique(labels).size - 1
+
+
---
+++ tutorial/scikit/image_lesson.py
@@ -0,0 +1,162 @@
+# -*- coding: utf-8 -*-
+
+'''
+
+Этот файл cодержит последовательность команд, которые необходимо выполнять в
+интерактивном режиме. Не пытайтесь его запускать или импортировать: это не
+имеет смысла.
+
+'''
+
+import matplotlib.pyplot as plt
+import numpy as np
+plt.ion()                 # включение интерактивного режима matplotlib
+from image_funcs import * # импорт всех функций из файла image_funcs.py
+from scipy.misc import imread, imsave
+
+###############################################################################
+####  Чтение, запись, визуализация и преобразование в ч/б формат тестового ####
+####  рисунка                                                              ####
+###############################################################################
+nuclei = imread('test_dna.jpg')
+nuclei.shape
+(1024, 1360, 3)
+nuclei = np.max(nuclei, 2)
+nuclei.shape
+(1024, 1360)
+plt.imshow(nuclei)
+plt.gray()
+imsave('nuclei.jpg', nuclei)
+################################################################################
+
+
+################################################################################
+#### Перерастягивание интенсивности пикселей с использованием всего         ####
+#### допустимого диапазона значений (от 0 до 255)                           ####
+################################################################################
+from skimage.exposure import rescale_intensity
+rescaled_nuclei = rescale_intensity(nuclei, in_range=(np.min(nuclei),np.max(nuclei)))
+plt.imshow(rescaled_nuclei)
+
+new_range = tuple(np.percentile(nuclei,(2,98)))
+rescaled_nuclei = rescale_intensity(nuclei, in_range=new_range)
+plt.imshow(rescaled_nuclei)
+################################################################################
+
+
+################################################################################
+#### Применение фильтров, создание рисунка с повышенной чёткостью границ    ####
+################################################################################
+from skimage.filter import gaussian_filter
+blured = gaussian_filter(nuclei,8)
+plt.imshow(blured)
+
+highpass = nuclei - 0.8*blured
+sharp = highpass + nuclei
+sharp = np.floor(sharp).astype(np.uint8)
+
+fig, ax = plt.subplots(1,2)
+plt.gray()
+ax[0].imshow(nuclei)
+ax[1].imshow(sharp)
+################################################################################
+
+
+################################################################################
+#### Бинаризация изображений (белые клетки, чёрный фон)                     ####
+################################################################################
+# Алгоритм Отсу (первого порядка)
+from skimage.filter import threshold_otsu
+thres = threshold_otsu(rescaled_nuclei)
+binary = rescaled_nuclei > thres
+plt.imshow(binary)
+
+# Алгоритм кэнни (второго порядка)
+from skimage.filter import canny
+edges = canny(sharp, sigma = 1, high_threshold = 35., low_threshold = 14.)
+plt.imshow(edges)
+
+# Неудачный способ заливки границ
+from scipy.ndimage import binary_fill_holes
+binary = binary_fill_holes(edges)
+plt.imshow(binary)
+
+# Правильный способ заливки границ
+from skimage.morphology import binary_dilation, binary_erosion
+diamond = np.array([0,1,0,1,1,1,0,1,0], dtype=bool).reshape((3,3))
+
+edges = double_dilation(edges, diamond)
+plt.imshow(edges)
+
+binary = fill_holes(edges)
+plt.imshow(binary)
+
+binary = double_erosion(binary, diamond)
+plt.imshow(binary)
+################################################################################
+
+
+################################################################################
+#### Нумерация, сегментация, удаление малых объектов и объектов на границе  ####
+#### изображения                                                            ####
+################################################################################
+from skimage.measure import label
+labels = label(binary)
+plt.jet()
+plt.imshow(colorize(labels))
+
+from skimage.morphology import remove_small_objects
+labels = remove_small_objects(labels, 4000)
+labels = clear_border(labels)
+plt.imshow(labels)
+
+binary = labels != 0
+plt.imshow(binary)
+
+from scipy.ndimage import distance_transform_edt
+from skimage.feature import peak_local_max
+from skimage.morphology import watershed
+from image_funcs import colorize
+
+# Неудачный способ сегментации
+distance = distance_transform_edt(binary)
+local_maxi = peak_local_max(distance, indices=False, labels=binary, min_distance = 10)
+markers = label(local_maxi)
+plt.imshow(double_dilation(local_maxi,diamond))
+labels_ws = watershed(-distance, markers, mask=binary)
+plt.imshow(сщдщкшяу(labels_ws))
+
+
+# Правильный способ сегментации
+distance_blured = gaussian_filter(distance, 8)
+local_maxi = peak_local_max(distance_blured, indices=False, labels=binary, min_distance = 10)
+markers = label(local_maxi)
+labels_ws = watershed(-distance, markers, mask=binary)
+plt.imshow(colorize(labels_ws))
+
+# Подсчёт числа клеток
+cell_number = np.unique(labels_ws).size - 1
+print cell_number
+################################################################################
+
+
+################################################################################
+#### Подсчёт интенсивности флуоресценции клеток, найденных ранее,           ####
+#### и построение столбчатой диаграммы                                      ####
+################################################################################
+protein = imread('test_protein.jpg')
+protein = np.max(protein,2)
+plt.imshow(protein)
+prot_int = calc_prot_intensity(labels_ws,protein)
+print np.round(prot_int,2)
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+n, bins, patches = ax.hist(prot_int, 10, normed=1, facecolor=(0.6,1. ,0.4), alpha=0.75)
+ax.set_ylabel('Cell fraction')
+ax.set_xlabel('Mean intensity')
+################################################################################
+
+
+
+
